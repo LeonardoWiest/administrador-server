6efc7ea98193742000a72ff32ee5d8f3a849c8bb
Leonardo Wiest <leonardowiest@hotmail.com>
2019-07-11 21:18:47 -0300

    Alterado changeSet e entidade Usuario

--- src/main/java/com/github/leonardowiest/administrador/domain/Usuario.java
+++ src/main/java/com/github/leonardowiest/administrador/domain/Usuario.java
@@ -4,7 +4,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -12,6 +15,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -21,22 +25,25 @@ import lombok.Data;
 @Entity
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
+@SequenceGenerator(name = "gen_usuario", sequenceName = "gen_usuario", allocationSize = 1)
 @Table(name = "tb_usuario")
 public class Usuario {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen_usuario")
 	@Column(name = "id_usuario", nullable = false)
 	private Long id;
 
-	@Column(name = "tx_nome", nullable = true)
+	@Column(name = "tx_nome", nullable = false, length = 80)
 	private String nome;
 
-	@Column(name = "tx_login", nullable = false)
+	@Column(name = "tx_login", nullable = false, length = 30)
 	private String login;
 
-	@Column(name = "tx_senha", nullable = false)
+	@Column(name = "tx_senha", nullable = false, length = 32)
 	private String senha;
 
+	@ColumnDefault("N")
 	@Column(name = "fl_superusuario", nullable = true)
 	private String flagSuperUsuario;
 
@@ -50,7 +57,7 @@ public class Usuario {
 	@UpdateTimestamp
 	private Date dataManutencao;
 
-	@Column(name = "dt_exclusao", nullable = true)
+	@Column(name = "dt_exclusao")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataExclusao;
 
