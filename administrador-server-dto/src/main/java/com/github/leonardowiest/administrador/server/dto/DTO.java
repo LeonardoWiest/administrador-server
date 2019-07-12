3871f0e51bfd86522f96089373c155499bd3f257
Leonardo Wiest <leonardowiest@hotmail.com>
2019-07-11 22:58:33 -0300

    Modularizado projeto

---
+++ administrador-server-dto/src/main/java/com/github/leonardowiest/administrador/server/dto/DTO.java
@@ -0,0 +1,9 @@
+package com.github.leonardowiest.administrador.server.dto;
+
+import java.io.Serializable;
+
+public class DTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ administrador-server-util/src/main/java/com/github/leonardowiest/administrador/server/util/TesteUtil.java
@@ -0,0 +1,5 @@
+package com.github.leonardowiest.administrador.server.util;
+
+public final class TesteUtil {
+
+}
