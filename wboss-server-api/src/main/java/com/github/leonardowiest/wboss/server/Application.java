36c1a3c96f916a645e680b39cf2aabd2d726f43a
"Leonardo Wiest" <leonardowiest@hotmail.com>
2019-07-24 22:16:23 -0300

    Colocado annotation

--- wboss-server-api/src/main/java/com/github/leonardowiest/wboss/server/Application.java
+++ wboss-server-api/src/main/java/com/github/leonardowiest/wboss/server/Application.java
@@ -1,11 +1,9 @@
 package com.github.leonardowiest.wboss.server;
 
-import static com.github.leonardowiest.wboss.server.util.constants.GlobalConstants.PKG_API;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { PKG_API })
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
